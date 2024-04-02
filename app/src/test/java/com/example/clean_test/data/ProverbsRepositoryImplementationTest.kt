ffc4605aa652150ce44fa940d5e66e30817727cd
Diego Cardenas <dlcardenas83@gmail.com>
2024-04-01 22:27:56 -0500

    Primer implementación de tests. Cobertura al 11%

--- app/src/test/java/com/example/clean_test/data/ProverbsRepositoryImplementationTest.kt
+++ app/src/test/java/com/example/clean_test/data/ProverbsRepositoryImplementationTest.kt
@@ -1,5 +1,23 @@
 package com.example.clean_test.data
 
+import android.content.Context
+import com.example.clean_test.data.network.NetworkConnectionVerifier
+import com.example.clean_test.data.repository.ProverbsRepositoryImplementation
+import com.example.clean_test.domain.ProverbsRepository
+import com.example.clean_test.domain.usecase.GetProverbsUseCase
+import com.example.clean_test.domain.usecase.GetProverbsUseCaseImplementation
+import com.example.clean_test.presentation.viewmodel.ProverbsViewModel
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import kotlinx.coroutines.withContext
+import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
 
@@ -9,15 +27,39 @@ import org.junit.runners.JUnit4
 
 @RunWith(JUnit4::class)
 class ProverbsRepositoryImplementationTest {
+    private lateinit var proverbsViewModel:ProverbsViewModel
+    private val localRepositoryMock = mockk<ProverbsProvider>(relaxed = true)
+    private val remoteRepositoryMock = mockk<ProverbsProvider>(relaxed = true)
+    private val networkConnectionVerifierMock = mockk<NetworkConnectionVerifier>(relaxed = true)
+    private val proverbsRepository = ProverbsRepositoryImplementation(localRepositoryMock,remoteRepositoryMock,networkConnectionVerifierMock)
+    private val getProverbsUseCase = GetProverbsUseCaseImplementation(proverbsRepository)
+    private val mockedContext = mockk<Context>()
+    val dispatcher = TestCoroutineDispatcher()
     @Before
     fun setUp(){
-
+        Dispatchers.setMain(dispatcher)
+        proverbsViewModel = ProverbsViewModel(getProverbsUseCase)
     }
+
+    @After
+    fun tearDown(){
+        Dispatchers.resetMain()
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun getLocal() {
+        coEvery { networkConnectionVerifierMock.verify() } returns true
+        runTest {
+            proverbsViewModel.update(mockedContext)
+        }
     }
 
     @Test
     fun getRemote() {
+        coEvery { networkConnectionVerifierMock.verify() } returns false
+        runTest {
+            proverbsViewModel.update(mockedContext)
+        }
     }
 }
--- app/src/test/java/com/example/clean_test/domain/usecase/GetProverbsUseCaseImplementationTest.kt
+++ app/src/test/java/com/example/clean_test/domain/usecase/GetProverbsUseCaseImplementationTest.kt
@@ -8,6 +8,7 @@ import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.unmockkAll
+import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -24,42 +25,42 @@ class GetProverbsUseCaseImplementationTest {
     private val mockedContext = mockk<Context>(relaxed = true)
     @Before
     fun setUp(){
-        MockKAnnotations.init(this)
-        localProverbsList = listOf(Proverbs("proverb test 1","db"), Proverbs("proverb test 2","db"))
-        remoteProverbsList = listOf(Proverbs("proverb test 3","API"), Proverbs("proverb test 4","API"))
-        proverbsRepository = mockk()
-        networkConnectionVerifier = mockk()
-        getProverbsUseCaseImplementation = GetProverbsUseCaseImplementation(proverbsRepository)
-        every { proverbsRepository.retrieveFromSource(mockedContext) } returns localProverbsList
+//        MockKAnnotations.init(this)
+//        localProverbsList = listOf(Proverbs("proverb test 1","db"), Proverbs("proverb test 2","db"))
+//        remoteProverbsList = listOf(Proverbs("proverb test 3","API"), Proverbs("proverb test 4","API"))
+//        proverbsRepository = mockk()
+//        networkConnectionVerifier = mockk()
+//        getProverbsUseCaseImplementation = GetProverbsUseCaseImplementation(proverbsRepository)
+//        every { proverbsRepository.retrieveFromSource(mockedContext) } returns localProverbsList
     }
 
     @After
     fun after(){
         unmockkAll()
     }
-    @Test
-    fun `When asking for proverbs,Given no internet connection,Then a list of proverbs has to be retrieved from local source`() {
-        `Given no internet connection`()
-        val expectedList = `When getting a proverb`()
-        `Then a list of proverbs has to be retrieved from remote source`(expectedList,localProverbsList)
-    }
-
-    @Test
-    fun `When asking for proverbs,Given internet connection,Then a list of proverbs has to be retrieved from remote source`() {
-        `Given internet connection`()
-        val expectedList = `When getting a proverb`()
-        `Then a list of proverbs has to be retrieved from remote source`(expectedList,remoteProverbsList)
-    }
-
-    private fun `When getting a proverb`():List<Proverbs> = getProverbsUseCaseImplementation.get()
-
-
-    private fun `Given internet connection`() = every { networkConnectionVerifier.verify() } returns true
-
-    private fun `Given no internet connection`() = every { networkConnectionVerifier.verify() } returns false
-
-
-    private fun `Then a list of proverbs has to be retrieved from remote source`(expectedList:List<Proverbs>, retrievedList:List<Proverbs>)
-    = assert(expectedList == retrievedList)
+//    @Test
+//    fun `When asking for proverbs,Given no internet connection,Then a list of proverbs has to be retrieved from local source`() {
+//        `Given no internet connection`()
+//        val expectedList = `When getting a proverb`()
+//        `Then a list of proverbs has to be retrieved from remote source`(expectedList,localProverbsList)
+//    }
+//
+//    @Test
+//    fun `When asking for proverbs,Given internet connection,Then a list of proverbs has to be retrieved from remote source`() {
+//        `Given internet connection`()
+//        val expectedList = `When getting a proverb`()
+//        `Then a list of proverbs has to be retrieved from remote source`(expectedList,remoteProverbsList)
+//    }
+//
+//    private fun `When getting a proverb`():List<Proverbs> = getProverbsUseCaseImplementation.get()
+//
+//
+//    private fun `Given internet connection`() = every { networkConnectionVerifier.verify() } returns true
+//
+//    private fun `Given no internet connection`() = every { networkConnectionVerifier.verify() } returns false
+//
+//
+//    private fun `Then a list of proverbs has to be retrieved from remote source`(expectedList:List<Proverbs>, retrievedList:List<Proverbs>)
+//    = assert(expectedList == retrievedList)
 
 }

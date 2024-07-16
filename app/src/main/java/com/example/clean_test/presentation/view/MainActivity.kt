36866423b7196a491e7243e57bccfddb53db315a
Diego Cardenas <dlcardenas83@gmail.com>
2024-07-15 22:25:56 -0500

    First commit widget on UI view.

--- app/src/main/java/com/example/clean_test/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/clean_test/presentation/view/MainActivity.kt
@@ -4,6 +4,17 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.clean_test.databinding.ActivityMainBinding
 import com.example.clean_test.domain.entities.Proverbs
 import com.example.clean_test.presentation.viewmodel.ProverbsViewModel
@@ -13,6 +24,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val proverbsViewModel: ProverbsViewModel by viewModels()
     private lateinit var mainBinding: ActivityMainBinding
+    private lateinit var composeView: ComposeView
     companion object{
         const val ON_EMPTY_PROVERB_MESSAGE = "No proverbs available, please try again."
     }
@@ -20,9 +32,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        composeView = mainBinding.mainComposeContainer
         setContentView(mainBinding.root)
         initObservers()
         initListeners()
+        initComposeView()
+    }
+
+    private fun initComposeView(){
+        composeView.setContent {
+            Column(modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally) {
+                Spacer(modifier = Modifier.height(30.dp))
+                Text(text = "hola mundo", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            }
+        }
     }
 
     private fun initObservers() {
